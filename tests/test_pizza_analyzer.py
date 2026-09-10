d2dd69304c970c7a9e05784afcc5c880082de966
Negel Walker <walker421@aol.com>
2026-09-10 17:19:33 -0400

    Add initial pizza analyzer unit tests
    Add tests for normalize_toppings and get_top_combinations functions.

---
+++ tests/test_pizza_analyzer.py
@@ -0,0 +1,21 @@
+from scr.pizza_analyzer import normalize_toppings, get_top_combinations
+
+def test_normalize_toppings_sorts_toppings():
+  toppings = ["pepperoni", "mushrooms"]
+
+result = normalize_toppings(toppings)
+
+assert result == ("mushrooms, "pepperoni")
+
+def test_get_top_combinations_counts_identical_combinations():
+pizza_orders = [
+{"toppings": ["pepperoni", "mushrooms"]},
+{"toppings": ["mushrooms", "pepperoni"]},
+{"toppings": ["cheese"]},
+
+]
+
+result = get_top_combinations(pizza_orders)
+
+assert result [0] == (("mushrooms", "pepperoni"), 2)
+assert result [1] == (("cheese",), 1)
