bba2641d95d48619cadacebc75d8193a4e97587f
Negel Walker <walker421@aol.com>
2026-09-21 19:14:47 -0400

    Fix function name by replacing hyphen with underscore

--- tests/test_pizza_analyzer.py
+++ tests/test_pizza_analyzer.py
@@ -281,7 +281,7 @@ def test_get_top_combinations_ranks_ties_alphabetically():
 
     ]
 
-def test_get_top_combinations_ranks_multi-topping_ties_alphabetically():
+def test_get_top_combinations_ranks_multi_topping_ties_alphabetically():
     pizza_orders = [
         {"toppings": ["pepperoni", "mushrooms"]},
         {"toppings": ["sausage", "onions"]},
