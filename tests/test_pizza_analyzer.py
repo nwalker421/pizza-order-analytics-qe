03a3ab35df014d2ee288c0e1cd82642b58d64f87
Negel Walker <walker421@aol.com>
2026-09-25 17:44:59 -0400

    Correct variable name in get_top_combinations test
    Fix variable name for pizza orders in test.

--- tests/test_pizza_analyzer.py
+++ tests/test_pizza_analyzer.py
@@ -421,7 +421,7 @@ def test_get_combinations_applies_default_limit_of_20():
     ]
 
 #Call function without supplying a limit
-result = get_top_combinations(pizza_order)
+result = get_top_combinations(pizza_orders)
 
 #Verify the default maximum is 20
 assert len(result) == 20
