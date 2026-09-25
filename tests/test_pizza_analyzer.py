f2c72160d5f79ab77ece0ad782af90b9391292ba
Negel Walker <walker421@aol.com>
2026-09-25 16:50:51 -0400

    Fix syntax error in test_get_combination_returns_top_20

--- tests/test_pizza_analyzer.py
+++ tests/test_pizza_analyzer.py
@@ -387,8 +387,8 @@ def test_get_combinations_returns_all_when_fewer_than_20():
 
 def test_get_combination_returns_top_20_from_21_unique():
     pizza_orders = [
-        {"toppings": [f"topping{i:02d)"]}
-        for i in range(1,22)
+        {"toppings": [f"topping{i:02d}"]}
+        for i in range(1, 22)
 
     ]
 
